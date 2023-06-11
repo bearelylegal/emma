cfb014352f6005fb8c3e8a3904878a17dea18601
bearelylegal <landschreiber@web.de>
2023-06-11 11:05:01 +0200

    inital push

---
+++ app.js
@@ -0,0 +1,20 @@
+const year_born = 2023;
+const month_born = 2;
+const month_day = 10;
+
+function getAge(birthYear, month_born, month_day) {
+  let now = new Date();
+  let currentYear = now.getFullYear();
+  let currentMonth = now.getMonth() + 1;
+  let currentDay = now.getDate();
+  console.log(now);
+  console.log(now.getDate());
+  age = currentYear - birthYear;
+  month = currentMonth - month_born;
+  day = currentDay - month_day;
+  return [age, month, day];
+}
+
+getAge(year_born, month_born, month_day);
+document.getElementById("yearsold").innerHTML =
+  age + " y " + month + " m " + day + "d";
