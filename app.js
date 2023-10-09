42abbdda55dc24b8120994cb9f8f067db62a468e
bearelylegal <landschreiber@web.de>
2023-10-09 23:02:26 +0200

    correct date calc

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ const month_born = 2;
 const month_day = 10;
 
 function getAge(birthYear, month_born, month_day) {
-  let now = new Date();
-  let currentYear = now.getFullYear();
-  let currentMonth = now.getMonth() + 1;
-  let currentDay = now.getDate();
-  console.log(now);
-  console.log(now.getDate());
-  age = currentYear - birthYear;
-  month = currentMonth - month_born;
-  day = currentDay - month_day;
-  return [age, month, day];
+  let date1 = new Date(birthYear, month_born - 1, month_day);
+  let date2 = new Date();
+  let diff = new Date(date2.getTime() - date1.getTime());
+  
+  year = (diff.getUTCFullYear() - 1970); // Gives difference as year
+  
+  month = (diff.getUTCMonth()); // Gives month count of difference
+  
+  day = (diff.getUTCDate() - 1); // Gives day count of difference
+  return [year, month, day];
 }
 
 getAge(year_born, month_born, month_day);
 document.getElementById("yearsold").innerHTML =
-  age + " y " + month + " m " + day + "d";
+  year + " y " + month + " m " + day + "d";
 
 
 if (month === 0 && day === 0) {
 document.getElementById("emoji").innerHTML = "🎂";
-console.log("happy birthday emma, we love you so much ❤️");
+console.log("happy birthday emma emmchen, we love you so much ❤️");
 }
